ba0fe79f54368a1c2db457c69353ae9e11a4608e
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2025-05-27 10:25:44 +0300

    Embed cosmovisor upgrade manager in container (#20)
    In order to manage on-chain upgrades, build cosmovisor into the container
    image together with all node binaries so that single container can run
    through the whole chain from genesis to the HEAD.
    
    When bootstrapping a node from scratch, after an upgrade, using state-sync,
    the built-in cosmovisor setup in the container doesn't have any means to
    detect what's the correct version to start with.
    
    This change declares two environment variables, `$UPGRADE_VERSION` and
    `$UPGRADE_HEIGHT` which can be declared for state-sync bootstrap.
    
    The node will then start with the right node binary version and continue
    as if it was running since genesis.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -13,8 +13,25 @@ then
 EOF
 fi
 
-echo "Starting zkcloudd with command:"
-echo "/bin/zkcloudd $@"
-echo ""
+# Check if the `upgrade-info.json` file should be populated.
+if [[ -n "$UPGRADE_VERSION" && -n "$UPGRADE_HEIGHT" ]]; then
+  echo "Upgrade version env variables found:"
+  echo "  UPGRADE_VERSION=$UPGRADE_VERSION"
+  echo "  UPGRADE_HEIGHT=$UPGRADE_HEIGHT"
 
-exec /bin/zkcloudd $@
+  # Check if upgrade-info.json exists
+  if [[ ! -f upgrade-info.json ]]; then
+    echo "Creating upgrade-info.json."
+    echo "{\"name\":\"$UPGRADE_VERSION\",\"time\":\"0001-01-01T00:00:00Z\",\"height\":$UPGRADE_HEIGHT}" > "${ZKCLOUD_HOME}/data/upgrade-info.json"
+    echo "upgrade-info.json created."
+  else
+    echo "upgrade-info.json already exists. No action taken."
+  fi
+else
+  echo "UPGRADE_VERSION and/or UPGRADE_HEIGHT are not set or empty. Continuing with cosmovisor default behavior."
+fi
+
+
+cp -rvf /app/cosmovisor ${HOME}/.zkcloud/cosmovisor
+
+exec /bin/cosmovisor run "$@"
